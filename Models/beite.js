6f2a5aa16651e22b631f090cb5870897865d42ce
Splur <sebastianstabaek@gmail.com>
2025-04-11 08:37:47 +0200

    Sletta unødvendig fil
    Det var en modell fra et annet prosjekt.

--- Models/beite.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const beiteSchema = new mongoose.Schema({
-    primærområde: String,
-    fylke: String
-});
-
-module.exports = mongoose.model("beite", beiteSchema);
